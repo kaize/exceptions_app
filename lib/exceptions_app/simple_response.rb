c12492da7609c11fe2856a291c7a2b56bdcb306d
Igor Kuznetsov <igkuznetsov@gmail.com>
2012-12-26 15:17:43 +0400

    Modified exceptions_app middleware to load generated static files on exception

--- lib/exceptions_app/simple_response.rb
+++ lib/exceptions_app/simple_response.rb
@@ -8,8 +8,20 @@ module ExceptionsApp
     def call(env)
       status = env["PATH_INFO"][1..-1]
       @headers ||= {}
+      statics_path = "#{ExceptionsApp.config.statics_path}/#{status}.html"
+      p statics_path
 
-      [status, @headers.merge!({"X-Cascade" => "pass"}), []]
+      if File.exists?(statics_path)
+        render(status, File.read(statics_path))
+      else
+        [status, @headers.merge!({"X-Cascade" => "pass"}), []]
+      end
+    end
+
+    private
+    
+    def render(status, body)
+      [status, {'Content-Length' => body.bytesize.to_s}, [body]]
     end
   end
 end
